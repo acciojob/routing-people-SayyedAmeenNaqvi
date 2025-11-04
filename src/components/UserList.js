d9f5503da89d38441eda5994a0aec64ac649f117
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-11-04 20:03:35 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,28 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import 'regenerator-runtime/runtime'
 
-const UserList = () => {
-  const [users, setUsers] = useState([]);
+function UserList() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+    useEffect(() => {
+        async function fetchUsers() {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/users"
+            );
+            const data = await response.json();
+            setUsers(data);
+            setLoading(false)
+        }
 
-  return (
-    <div>
-      <h1>User List</h1>
-      <ul>
-        {users.length > 0 &&
-          users.map((user) => (
-            <li key={user.id}>
-              <Link to={`/users/${user.id}`}>{user.name}</Link>
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+        fetchUsers();
+    }, []);
+
+    if (loading) return (<h2>Loading...</h2>)
+
+    return (
+        <div>
+            <h1>User List</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <Link to={`/users/${user.id}`}>{user.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default UserList;
